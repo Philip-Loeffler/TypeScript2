492bc0de0f06ff3f19621c35f641604b429ceff8
Philip-Loeffler <Philiploeffler@gmail.com>
2018-12-11 16:54:26 -0500

    origin

---
+++ ts-types/types.ts
@@ -0,0 +1,15 @@
+//types in typscript
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number [] = [1,2,3];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color { Red = 0, Green = 1, Blue =2 };
+
+let backgroundColor = Color.Red;

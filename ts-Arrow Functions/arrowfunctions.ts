a2dd437c1e0e00ac7ed6e5d74c1205443599192a
Philip-Loeffler <Philiploeffler@gmail.com>
2018-12-12 13:00:50 -0500

    origin

---
+++ ts-Arrow Functions/arrowfunctions.ts
@@ -0,0 +1,11 @@
+// all the same. => is just a way to define functions
+let log = function(message) {
+    console.log(message);
+
+}
+
+let doLog = (message) => {
+    console.log(message);
+}
+
+let doLog2 = (message) => console.log(message);
